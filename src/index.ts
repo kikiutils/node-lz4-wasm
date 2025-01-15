11bf8654a7be2feb929f98555475120442ecb792
kiki-kanri <a470666@gmail.com>
2025-01-15 14:00:31 +0800

    fix: resolve issue with entry point not updating wasm loading method

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as wasmJs from '../rust/pkg/lz4_wasm';
 import wasmCore from '../rust/pkg/lz4_wasm_bg.wasm';
 
 // @ts-expect-error Ignore this error.
-wasmCore().then(({ instance }) => wasmJs.default(instance));
+wasmJs.default(wasmCore());
 export const compress = (data: Uint8Array) => wasmJs.compress(data);
 export const compressPrependSize = (data: Uint8Array) => wasmJs.compress_prepend_size(data);
 export const decompress = (data: Uint8Array, minUncompressedSize: number) => wasmJs.decompress(data, minUncompressedSize);
