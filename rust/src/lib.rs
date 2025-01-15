86aeaf0fadc4c662aa5f80b86c8c2cf0348c81bd
kiki-kanri <a470666@gmail.com>
2025-01-15 13:34:52 +0800

    feat: modify implemented and exported functions to correspond with lz4-flex functions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,13 +1,23 @@
-use lz4_flex::{compress_prepend_size, decompress_size_prepended};
 use wasm_bindgen::prelude::{JsValue, wasm_bindgen};
 
 #[wasm_bindgen]
 pub fn compress(data: &[u8]) -> Vec<u8> {
-    compress_prepend_size(data)
+    lz4_flex::compress(data)
 }
 
 #[wasm_bindgen]
-pub fn decompress(data: &[u8]) -> Result<Vec<u8>, JsValue> {
-    decompress_size_prepended(data)
+pub fn decompress(data: &[u8], min_uncompressed_size: usize) -> Result<Vec<u8>, JsValue> {
+    lz4_flex::decompress(data, min_uncompressed_size)
+        .map_err(|e| JsValue::from_str(&format!("Decompression error: {:#?}", e)))
+}
+
+#[wasm_bindgen]
+pub fn compress_prepend_size(data: &[u8]) -> Vec<u8> {
+    lz4_flex::compress_prepend_size(data)
+}
+
+#[wasm_bindgen]
+pub fn decompress_size_prepended(data: &[u8]) -> Result<Vec<u8>, JsValue> {
+    lz4_flex::decompress_size_prepended(data)
         .map_err(|e| JsValue::from_str(&format!("Decompression error: {:#?}", e)))
 }
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ import wasmCore from '../rust/pkg/lz4_wasm_bg.wasm';
 // @ts-expect-error Ignore this error.
 wasmCore().then(({ instance }) => wasmJs.default(instance));
 export const compress = (data: Uint8Array) => wasmJs.compress(data);
-export const decompress = (data: Uint8Array) => wasmJs.decompress(data);
+export const compressPrependSize = (data: Uint8Array) => wasmJs.compress_prepend_size(data);
+export const decompress = (data: Uint8Array, minUncompressedSize: number) => wasmJs.decompress(data, minUncompressedSize);
+export const decompressSizePrepended = (data: Uint8Array) => wasmJs.decompress_size_prepended(data);
